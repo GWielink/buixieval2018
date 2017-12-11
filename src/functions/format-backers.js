9c0e390311721420cba345f3897eaba3ea4a97b1
GWielink <g.wielink.1@student.rug.nl>
2017-12-11 15:18:15 +0100

    parse to float for better money handling...

--- src/functions/format-backers.js
+++ src/functions/format-backers.js
@@ -1,7 +1,7 @@
 export default function (backers, showAll) {
     return backers
         .map(backer => (Object.assign({}, backer, {
-            contributed: parseInt(backer.contributed, 10)
+            contributed: parseFloat(backer.contributed, 10)
         })))
         .filter( backer => ( showAll ? true : backer.contributed > 0))
         .sort((a, b) => (b.contributed - a.contributed));
