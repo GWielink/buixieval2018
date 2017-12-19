00a14ccd7e96dd4440a7e5610cdb9c4a19e9c141
GWielink <g.wielink.1@student.rug.nl>
2017-12-19 14:43:59 +0100

    split backerswall into teams

--- src/routes/Backers/index.js
+++ src/routes/Backers/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Grid from './Grid';
 import Menu from '../../components/Menu';
-import {backerDisplayDetails} from '../../store/actions';
+
 import './backer.css';
 import fetchBackers from '../../functions/fetch-backers';
 
@@ -22,15 +22,11 @@ class BackersContainer extends Component {
         this.getBackers();
     }
 
-    componentWillUnmount () {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-        this.props.clearDetails();
-    }
-
     updateWindowDimensions = () => {
         this.setState({
             width: window.innerWidth,
-            height: window.innerHeight,
+            height: window.innerHeight - 50,
+            direction: window.innerWidth > window.innerHeight ? 'row' : 'col'
         });
     };
 
@@ -45,15 +41,56 @@ class BackersContainer extends Component {
         });
     };
 
+    splitBackers = (backers) => {
+        const pink  = backers.filter(backer => (backer.team === 'p'));
+        const blue = backers.filter(backer => (backer.team === 'b'));
+
+        const pinkTotal = backers.reduce((total, backer) => (total + backer.contributed), 0);
+        const blueTotal = backers.reduce((total, backer) => (total + backer.contributed), 0);
+
+        const total = pinkTotal + blueTotal;
+        const pinkPartition = pinkTotal / total;
+        const bluePartition = blueTotal / total;
+
+
+        const pinkWidth = this.state.direction === 'col' ? this.state.width : this.state.width * pinkPartition;
+        const blueWidth = this.state.direction === 'col' ? this.state.width : this.state.width - pinkWidth;
+        const pinkHeight = this.state.direction === 'row' ? this.state.height : this.state.height * pinkPartition;
+        const blueHeight = this.state.direction === 'row' ? this.state.height : this.state.height - pinkHeight;
+        console.log('blue: ', blue);
+        console.log('blueWidht: ', blueWidth);
+        console.log('blueHeigth: ', blueHeight);
+        return ({
+            pink,
+            blue,
+            pinkHeight,
+            pinkWidth,
+            blueHeight,
+            blueWidth,
+        })
+    }
+
     render () {
+        const {pink, blue, pinkHeight, pinkWidth, blueHeight, blueWidth} = this.splitBackers(this.props.backers);
         return (
+            // <div>
+            //     <Menu />
+            //     <Grid
+            //         backers={this.props.backers}
+            //         height={this.state.height - 30}
+            //         width={this.state.width}
+            //     />
+            // </div>
             <div>
-                <Menu />
-                <Grid
-                    backers={this.props.backers}
-                    height={this.state.height - 50}
-                    width={this.state.width}
-                />
+                <Menu /> 
+                <div style={{ width: this.state.width, height: this.state.height, overflow: 'hidden'}}>
+                    <div style={{ display: this.state.direction === 'row' ? 'inline-block' : 'block', overflow: 'hidden'}}>
+                        <Grid backers={pink} width={pinkWidth} height={pinkHeight} />
+                    </div>
+                    <div style={{ display: this.state.direction === 'row' ? 'inline-block' : 'block', overflow: 'hidden'}}>
+                        <Grid backers={blue} width={blueWidth} height={blueHeight} />
+                    </div>
+                </div>
             </div>
         )
     }
@@ -63,8 +100,4 @@ const mapStateToProps = state => ({
     backers: state.backers,
 });
 
-const mapDispatchToProps = dispatch => ({
-    clearDetails: () => dispatch(backerDisplayDetails(null))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(BackersContainer);
+export default connect(mapStateToProps)(BackersContainer);
